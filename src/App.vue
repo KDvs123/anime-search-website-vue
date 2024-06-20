1fe849400aaf1b9555f9079a532615cf732dd87f
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-20 21:16:22 +0530

    added the search functionality and the styling for it

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,14 @@
   <div class="app">
     <header>
       <h1>The <strong>Anime</strong>Database</h1>
-
+      <form action="" class="search-box">
+        <input
+          type="search"
+          class="search-field"
+          placeholder="Search for an anime..."
+          required
+        />
+      </form>
     </header>
   </div>
 </template>
@@ -12,39 +19,75 @@ export default {};
 </script>
 
 <style>
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: 'Fira Sans', sans-serif;
+  font-family: "Fira Sans", sans-serif;
 }
 
-a{
+a {
   text-decoration: none;
 }
 
-header{
-  padding-top: 50px ;
-  padding-bottom:50px ;
+header {
+  padding-top: 50px;
+  padding-bottom: 50px;
 }
 
-header h1{
+header h1 {
   color: #888;
-  font-size:42px;
+  font-size: 42px;
   font-weight: 400;
   text-align: center;
   text-transform: uppercase;
+  margin-bottom: 30px;
+}
+
+h1 strong {
+  color: #313131;
+}
+
+h1:hover {
+  color: #313131;
+}
+
+.search-box{
+  display: flex;
+  padding-left: 30px;
+  padding-right: 30px;
+  justify-content: center;
+}
+
+.search-field{
+  appearance: none;
+  background: none;
+  border: none;
+  outline: none;
+  background-color:#f3f3f3 ;
+  box-shadow: 0px 4px 8px rgba(0,0,0,0.15);
+  display: block;
+  width: 100%;
+  max-width: 600px;
+  padding: 15px;
+  border-radius: 8px;
+  color: #313131;
+  font-size: 20px;
+  transition: 0.4s;
+
 
 }
 
-h1 strong{
-  color: #313131;
+.search-field::placeholder{
+  color: #aaa;
 
-    
 }
 
-h1:hover{
-  color: #313131;
+.search-field:focus,.search-field:valid{
+  color: #fff;
+  background-color: #313131;
+  box-shadow: 0px 0px 0px rgba(0,0,0,0.15);
+
 }
 
 
