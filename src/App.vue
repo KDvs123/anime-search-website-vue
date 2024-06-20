a42f21d595e2a8360660a6b4762d43a403fbe00d
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-20 21:08:54 +0530

    adding the vue boilter template and the styling

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="app">
+    <header>
+      <h1>The <strong>Anime</strong>Database</h1>
+
+    </header>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', sans-serif;
+}
+
+a{
+  text-decoration: none;
+}
+
+header{
+  padding-top: 50px ;
+  padding-bottom:50px ;
+}
+
+header h1{
+  color: #888;
+  font-size:42px;
+  font-weight: 400;
+  text-align: center;
+  text-transform: uppercase;
+
+}
+
+h1 strong{
+  color: #313131;
+
+    
+}
+
+h1:hover{
+  color: #313131;
+}
+
+
+</style>
